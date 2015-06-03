1a9c7eca1b2ea6c1b91e8609d3c28611faaaad03
Ye Ding <guiewy@gmail.com>
2015-06-03 23:42:41 +0800

    v0.6.3

--- projects/joker/js/data.js
+++ projects/joker/js/data.js
@@ -3,9 +3,23 @@ $(document).ready(function () {
     Layout.init(); // init layout
     QuickSidebar.init(); // init quick sidebar
     $("button[data-toggle='confirmation']").on('confirmed.bs.confirmation', function () {
-        $.get("data/delete.php?f=" + $(this).attr("filename"), function (r) {
-            if (r != "1") bootbox.alert("<span class='font-red'><i class='fa fa-warning'></i> Something is wrong while deleting the file!</span>");
-            else location.reload();
-        });
+        if ($(this).attr("op") == "delete") {
+            $.get("data/delete.php?f=" + $(this).attr("filename"), function (r) {
+                if (r != "1") bootbox.alert("<span class='font-red'><i class='fa fa-warning'></i> Something is wrong while deleting the file!</span>");
+                else location.reload();
+            });
+        } else if ($(this).attr("op") == "feature") {
+            bootbox.dialog({
+                message: "<img src='assets/global/img/loading-spinner-grey.gif' class='loading'><span>&nbsp;&nbsp;Loading...</span>",
+                closeButton: false
+            });
+            $.get(API_SERVER + "cust/add_cust_from_csv/?src=" + $(this).attr("filename"), function (r) {
+                bootbox.hideAll();
+                bootbox.alert(r);
+            }).fail(function () {
+                bootbox.hideAll();
+                bootbox.alert("<span class='font-red'><i class='fa fa-warning'></i> Something is wrong while processing the file!</span>");
+            });
+        }
     });
 });
