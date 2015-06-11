922d614163f088df12849b0ee5a8d541f54328df
Ye Ding <guiewy@gmail.com>
2015-06-11 19:57:24 +0800

    v0.6.11

--- projects/joker/js/feature.js
+++ projects/joker/js/feature.js
@@ -382,71 +382,68 @@ function barset(src, table_title, table_desc, label1, label2) {
         });
 }
 
-function histogram(column, categorical, table_title, table_desc, DATA_SRC) {
+function histogram(column, categorical, table_title, table_desc) {
     var figid = guid();
     $("#figure_container").append("<div style='display:inline-block;text-align:center;'><span class='bold'>" + table_title + "</span><br/><span>" + table_desc + "</span><div id='figure_container_" + figid + "'></div></div>");
-    $.get("python/api.py?CTL=105&SCHEMA=" + DATA_SRC + "&COLUMN=" + column + "&CATEGORICAL=" + categorical, function (data) {
-        data = eval("(" + data + ")");
-        if (data.status != 200) $("#figure_container_" + figid).html("Loading schema '" + column + "' failed.<br/>Error message: " + data.content);
-        else {
-            data = data.content;
-            var src = [];
-            for (var i = 0; i < data.hist.length; i++) {
-                src.push({
-                    "hist": data.hist[i],
-                    "bin_edges": Number(data.bin_edges[i]) === data.bin_edges[i] ? data.bin_edges[i].toFixed(2) : data.bin_edges[i]
-                });
-            }
-            var margin = {top: 20, right: 10, bottom: 70, left: 50},
-                width = 300 - margin.left - margin.right,
-                height = 300 - margin.top - margin.bottom;
-            var x = d3.scale.ordinal().rangeRoundBands([0, width], .1);
-            var y = d3.scale.linear().range([height, 0]);
-            var xAxis = d3.svg.axis().scale(x).orient("bottom");
-            var yAxis = d3.svg.axis().scale(y).orient("left").ticks(10, "%");
-            var svg = d3.select("#figure_container_" + figid).append("svg")
-                .attr("width", width + margin.left + margin.right)
-                .attr("height", height + margin.top + margin.bottom)
-                .append("g")
-                .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-            x.domain(src.map(function (d) {
-                return d.bin_edges;
-            }));
-            y.domain([d3.min(src, function (d) {
-                return d.hist;
-            }), d3.max(src, function (d) {
-                return d.hist;
-            })]);
-            svg.append("g")
-                .attr("class", "x axis")
-                .attr("transform", "translate(0," + height + ")")
-                .call(xAxis)
-                .selectAll("text")
-                .style("text-anchor", "end")
-                .attr("dx", "-.8em")
-                .attr("dy", "-.3em")
-                .attr("transform", function (d) {
-                    return "rotate(-65)";
-                });
-            svg.append("g")
-                .attr("class", "y axis")
-                .call(yAxis)
-                .append("text")
-                .attr("transform", "rotate(-90)");
-            svg.selectAll(".bar")
-                .data(src)
-                .enter().append("rect")
-                .attr("class", "bar")
-                .attr("x", function (d) {
-                    return x(d.bin_edges);
-                })
-                .attr("width", x.rangeBand())
-                .attr("y", function (d) {
-                    return y(d.hist);
-                })
-                .attr("height", function (d) {
-                    return height - y(d.hist);
-                });
+    $.get(API_SERVER + "joker/api/cust/histogram/?column=" + column + "&categorical=" + categorical, function (data) {
+        var src = [];
+        for (var i = 0; i < data.hist.length; i++) {
+            src.push({
+                "hist": data.hist[i],
+                "bin_edges": Number(data.bin_edges[i]) === data.bin_edges[i] ? data.bin_edges[i].toFixed(2) : data.bin_edges[i]
+            });
         }
+        var margin = {top: 20, right: 10, bottom: 70, left: 50},
+            width = 300 - margin.left - margin.right,
+            height = 300 - margin.top - margin.bottom;
+        var x = d3.scale.ordinal().rangeRoundBands([0, width], .1);
+        var y = d3.scale.linear().range([height, 0]);
+        var xAxis = d3.svg.axis().scale(x).orient("bottom");
+        var yAxis = d3.svg.axis().scale(y).orient("left").ticks(10, "%");
+        var svg = d3.select("#figure_container_" + figid).append("svg")
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+            .append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+        x.domain(src.map(function (d) {
+            return d.bin_edges;
+        }));
+        y.domain([d3.min(src, function (d) {
+            return d.hist;
+        }), d3.max(src, function (d) {
+            return d.hist;
+        })]);
+        svg.append("g")
+            .attr("class", "x axis")
+            .attr("transform", "translate(0," + height + ")")
+            .call(xAxis)
+            .selectAll("text")
+            .style("text-anchor", "end")
+            .attr("dx", "-.8em")
+            .attr("dy", "-.3em")
+            .attr("transform", function (d) {
+                return "rotate(-65)";
+            });
+        svg.append("g")
+            .attr("class", "y axis")
+            .call(yAxis)
+            .append("text")
+            .attr("transform", "rotate(-90)");
+        svg.selectAll(".bar")
+            .data(src)
+            .enter().append("rect")
+            .attr("class", "bar")
+            .attr("x", function (d) {
+                return x(d.bin_edges);
+            })
+            .attr("width", x.rangeBand())
+            .attr("y", function (d) {
+                return y(d.hist);
+            })
+            .attr("height", function (d) {
+                return height - y(d.hist);
+            });
+    }).fail(function () {
+        $("#figure_container_" + figid).html("Loading schema '" + column + "' failed.<br/>Error message: " + data.content);
     });
 }
