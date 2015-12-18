f60ef73f9d9452469d1fa64e0aab3494ce4c8d6f
Ye Ding <guiewy@gmail.com>
2015-12-18 21:40:02 +0800

    v0.12.18

--- dashboard.php
+++ dashboard.php
@@ -31,7 +31,7 @@
             <div class="row">
                 <div class="col-md-12">
                     <h3>Updates</h3>
-                    <span class="font-blue">v0.12.7</span>
+                    <span class="font-blue">v0.12.18</span>
                     <ul>
                         <li><span class='font-yellow'>Modified</span> api system architecture</li>
                         <li><span class='font-purple'>Fixed</span> footer certificate link bug</li>
